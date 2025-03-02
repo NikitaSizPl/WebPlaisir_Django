b460cf6117d7e11be126ad7f7c1c4187a972a551
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-03-02 20:57:31 +0100

    create admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Catigories, Product
+from basket.models import Basket
 
 # Register your models here.
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'image']
+    list_editable = ['description', 'image']
+    prepopulated_fields = {'name': ('name',)}
+    ordering = ('id',)
+admin.site.register(Catigories, CategoriesAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'image']
+    list_editable = ['description', 'price', 'image']
+    prepopulated_fields = {'name': ('name',)}
+    ordering = ('id',)
+admin.site.register(Product, ProductAdmin)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = None
