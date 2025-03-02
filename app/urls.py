ea63f36067b8f9f564041ac41cdbc43bdcf59710
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-03-02 22:49:41 +0100

    add path 'user', 'basket', 'orders'

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for app project.
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+    path('user/', include('user.urls')),
+    path('basket/', include('basket.urls')),
+    path('orders/', include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
