108c01669336821c64cbd3ca6b82d856a79507ea
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2025-03-02 22:17:42 +0100

    create admin.py

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Basket, BasketItem
+
+class BasketItemInline(admin.TabularInline):
+    model = BasketItem
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'total_price']
+    inlines = [BasketItemInline]
 
 # Register your models here.
+admin.site.register(Basket, BasketAdmin)
